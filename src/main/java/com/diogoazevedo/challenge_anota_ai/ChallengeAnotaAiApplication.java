b9e13b93d420e6cadb8ff53d18e03522b4f3b61b
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-06-24 19:53:05 +0100

    init: generated code from spring initializer

---
+++ src/main/java/com/diogoazevedo/challenge_anota_ai/ChallengeAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.diogoazevedo.challenge_anota_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAnotaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diogoazevedo/challenge_anota_ai/ChallengeAnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogoazevedo.challenge_anota_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
