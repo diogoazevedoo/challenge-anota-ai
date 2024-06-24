d8c21af9e0f59cfdbcf9db36d24433c9acfcb327
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-06-24 20:03:00 +0100

    feat(config): add mongoDB config package

--- src/main/java/com/diogoazevedo/challenge_anota_ai/ChallengeAnotaAiApplication.java
+++ src/main/java/com/diogoazevedo/challenge_anota_ai/ChallengeAnotaAiApplication.java
@@ -2,8 +2,10 @@ package com.diogoazevedo.challenge_anota_ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ChallengeAnotaAiApplication {
 
 	public static void main(String[] args) {
